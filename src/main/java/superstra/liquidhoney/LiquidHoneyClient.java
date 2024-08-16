9e05f1c4600a3ada509a2e2df7d7f3eb5c6bd359
superstra <aidan.sheehan@gmail.com>
2024-08-16 15:51:28 -0600

    Upload template of mod

---
+++ src/main/java/superstra/liquidhoney/LiquidHoneyClient.java
@@ -0,0 +1,10 @@
+package superstra.liquidhoney;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiquidHoneyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/superstra/liquidhoney/LiquidHoneyDataGenerator.java
@@ -0,0 +1,11 @@
+package superstra.liquidhoney;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LiquidHoneyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
