059fe7b7216317b5e753aa4181f6adcb45a9228b
superstra <113485943+superstra@users.noreply.github.com>
2026-03-13 15:54:47 -0600

    Finished setup
    Finished setup. Setup thanks to Kaupenjoe's Fabric modding tutorial: https://youtu.be/oU8-qV-ZtUY?si=KeYZUdGkAX6aGITC

--- src/main/kotlin/com/superstra/liquidhoney/LiquidHoney.kt
+++ src/main/kotlin/com/superstra/liquidhoney/LiquidHoney.kt
@@ -4,12 +4,13 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object LiquidHoney : ModInitializer {
-    private val logger = LoggerFactory.getLogger("liquid-honey")
+	private val MOD_ID = "liquid-honey"
+    private val LOGGER = LoggerFactory.getLogger(MOD_ID)
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		LOGGER.info("Hello Fabric world!")
 	}
 }
---
+++ src/main/kotlin/com/superstra/liquidhoney/LiquidHoneyClient.kt
@@ -0,0 +1,9 @@
+package com.superstra.liquidhoney
+
+import net.fabricmc.api.ClientModInitializer
+
+class LiquidHoneyClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
